17deb8980962c8c9068ec7e4f4a06837970842d9
Raween <raween.kularathna@gmail.com>
2026-04-04 10:51:34 +1000

    Public IP inserted

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5001/api',
+  baseURL: 'http://15.135.194.34:5001/api',
 });
 
 api.interceptors.request.use((config) => {
