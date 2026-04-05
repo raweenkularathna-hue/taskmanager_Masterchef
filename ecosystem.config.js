6487bbe5a73cbf79382aaa5d9188a9f47f06a5f9
Raween <raween.kularathna@gmail.com>
2026-04-05 11:52:56 +1000

    Fix deploy stability ci.yml

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,8 @@ module.exports = {
   apps: [
     {
       name: 'backend',
-      script: './backend/server.js',
+      cwd: './backend',
+      script: 'server.js',
       watch: false,
       env: {
         NODE_ENV: 'production',
