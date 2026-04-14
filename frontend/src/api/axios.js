f95ce4e87472803947647a9d8b5d16f7c41c74ce
Raween <raween.kularathna@gmail.com>
2026-04-14 11:09:23 +1000

    IP changed 02

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://3.25.95.156:5001/api',
+  baseURL: 'http://13.210.246.25:5001/api',
 });
 
 api.interceptors.request.use((config) => {
